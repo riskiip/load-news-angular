954a0067eae5e3b4a6ba57add15ed2a44cfa126f
riskiip <muhamadrizki19@student.undip.ac.id>
2020-08-27 15:33:51 +0700

    Sudah bisa load news

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NewsService } from "src/app/services/news.service";
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.scss"],
 })
 export class NewsComponent implements OnInit {
+  articles: Array<any>;
 
-  constructor() { }
+  constructor(private news: NewsService) {}
 
   ngOnInit() {
+    this.getAllNews();
   }
 
+  getAllNews() {
+    return this.news.getNews().subscribe((data) => {
+      this.articles = data['articles'];
+    });
+  }
+
+  // searchNews(source: string) {
+  //   return this.news.getNewsById(source).subscribe((data) => {
+  //     this.articles = data["article"];
+  //   });
+  // }
 }
