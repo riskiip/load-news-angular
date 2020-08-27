0b09313eaec673cee2bd10c0f72c80abd0904b7c
riskiip <muhamadrizki19@student.undip.ac.id>
2020-08-27 14:58:36 +0700

    Routing ke home dan news sudah bisa

--- src/app/material-module/material-module.module.ts
+++ src/app/material-module/material-module.module.ts
@@ -3,12 +3,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
 
 const ModuleComponent = [
   MatToolbarModule,
   MatIconModule,
   MatButtonModule,
   MatMenuModule,
+  MatCardModule
 ];
 
 @NgModule({
