beae35329768d5750e13466b6630381460c50e42
riskiip <muhamadrizki19@student.undip.ac.id>
2020-09-04 13:59:44 +0700

    Finishing project

--- src/app/material-module/material-module.module.ts
+++ src/app/material-module/material-module.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatCardModule } from '@angular/material/card';
+import { NgModule } from "@angular/core";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatCardModule } from "@angular/material/card";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
 
 const ModuleComponent = [
   MatToolbarModule,
   MatIconModule,
   MatButtonModule,
   MatMenuModule,
-  MatCardModule
+  MatCardModule,
+  MatSidenavModule,
+  MatListModule,
 ];
 
 @NgModule({
